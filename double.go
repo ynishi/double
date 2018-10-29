5e85d4becdba5371983caa5569a79ebded28a908
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-10-30 01:06:13 +0900

    apply gofmt

--- double.go
+++ double.go
@@ -7,34 +7,34 @@ import (
 	"time"
 )
 
-func handlerFunc (sema chan struct{}, err chan error) func (w http.ResponseWriter, r *http.Request) {
-   return func (w http.ResponseWriter, r *http.Request) {
-	   fmt.Printf("start handle\n")
-	   if len(sema) == 1 {
-	   	    err <- errors.New("busy")
-		   fmt.Fprintf(w, "busy, cap:%d, len:%d", cap(sema), len(sema))
-		   return
-	   } else {
-	   	   // lock
-		   sema <- struct{}{}
-		   go second(sema, err)
-		   fmt.Fprintf(w, "sent, cap:%d, len:%d", cap(sema), len(sema))
-		   return
-	   }
-   }
+func handlerFunc(sema chan struct{}, err chan error) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("start handle\n")
+		if len(sema) == 1 {
+			err <- errors.New("busy")
+			fmt.Fprintf(w, "busy, cap:%d, len:%d", cap(sema), len(sema))
+			return
+		} else {
+			// lock
+			sema <- struct{}{}
+			go second(sema, err)
+			fmt.Fprintf(w, "sent, cap:%d, len:%d", cap(sema), len(sema))
+			return
+		}
+	}
 }
 
 type Second struct {
 	Sema chan struct{}
-	Err chan error
+	Err  chan error
 }
 
 func second(sema chan struct{}, err chan error) {
-		fmt.Println("second job start", time.Now())
-		time.Sleep(10 * time.Second)
-		fmt.Println("second job end", time.Now())
-		// unlock
-		<- sema
+	fmt.Println("second job start", time.Now())
+	time.Sleep(10 * time.Second)
+	fmt.Println("second job end", time.Now())
+	// unlock
+	<-sema
 }
 
 func errFunc(errC chan error) {
@@ -48,7 +48,7 @@ func main() {
 	s.Sema = make(chan struct{}, 1)
 	s.Err = make(chan error)
 
-    go errFunc(s.Err)
+	go errFunc(s.Err)
 
 	handler := handlerFunc(s.Sema, s.Err)
 	http.HandleFunc("/", handler)
